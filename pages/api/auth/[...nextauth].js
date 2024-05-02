90b42d9844641727d4a653b9d778e64582a195ea
pabel183 <jhpabel183@gmail.com>
2024-05-02 18:41:58 +0600

    netxtauth is modified for get and post method

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import authOptions from "@/lib/authOptions";
 
-export default NextAuth(authOptions);
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST };
+// export default NextAuth(authOptions);
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
